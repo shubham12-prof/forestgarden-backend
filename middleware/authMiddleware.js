a92888189268611561ea05a4f027ca6cfb34b4f5
shubham12-prof <shubhsemwal12@gmail.com>
2025-04-21 01:09:41 +0530

    admin access

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -8,6 +8,11 @@ const authMiddleware = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id).select("-password");
+
+    if (!req.user.isAdmin) {
+      return res.status(403).json({ message: "Access denied. Admins only." });
+    }
+
     next();
   } catch (err) {
     res.status(401).json({ message: "Token failed" });
