0e81e0e7b9b4db110bd2c47884ea96f3a1066f57
shubham12-prof <shubhsemwal12@gmail.com>
2025-04-21 01:42:31 +0530

    admin check

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,32 +1,27 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Not authorized" });
 
-const authMiddleware = (roleRequired = null) => {
-  return async (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) return res.status(401).json({ message: "Not authorized" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password");
 
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const user = await User.findById(decoded.id).select("-password");
-
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-
-      req.user = user;
-
-      if (roleRequired && req.user.role !== roleRequired) {
-        return res.status(403).json({
-          message: `Access denied, only ${roleRequired}s can perform this action`,
-        });
-      }
-
-      next();
-    } catch (err) {
-      res.status(401).json({ message: "Token failed" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  };
+
+    req.user = user;
+
+    if (!req.user.isAdmin) {
+      return res.status(403).json({
+        message: "Access denied, only admins can perform this action",
+      });
+    }
+
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token failed" });
+  }
 };
 
 module.exports = authMiddleware;
