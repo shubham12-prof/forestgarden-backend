2f3abf9abd87cdc9e80f66a6935578f3033432b2
shubham12-prof <shubhsemwal12@gmail.com>
2025-04-20 19:18:24 +0530

    authmiddleware change

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,24 +2,15 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
-  if (!token) {
-    return res
-      .status(401)
-      .json({ message: "Access denied. No token provided." });
-  }
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Not authorized" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
-    if (!user) {
-      return res.status(401).json({ message: "User not found." });
-    }
-    req.user = user;
+    req.user = await User.findById(decoded.id).select("-password");
     next();
   } catch (err) {
-    console.error("Error during token verification:", err);
-    res.status(401).json({ message: "Invalid or expired token" });
+    res.status(401).json({ message: "Token failed" });
   }
 };
 
