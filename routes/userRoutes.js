a754c3a3a768b6c4670b9d65f820e81f0d1fc5b2
shubham12-prof <shubhsemwal12@gmail.com>
2025-04-20 19:32:05 +0530

    edit route change

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,5 +16,6 @@ router.get("/my-children", authMiddleware, getMyChildren);
 router.get("/tree", authMiddleware, getUserTree);
 router.get("/:id", authMiddleware, getUserById);
 router.delete("/delete/:userId", authMiddleware, deleteUser);
-router.put("/users/:id", authMiddleware, updateUser);
+router.put("/:id", authMiddleware, updateUser);
+
 module.exports = router;
